0d1ffbc9c56f9e4a104107b81d165583d50af4bd
kawsark <kawsar@hashicorp.com>
2018-11-29 01:39:31 -0500

    added metadata display to web client

--- client.py
+++ client.py
@@ -25,6 +25,22 @@ def get_listings():
     except requests.ConnectionError as e:
         return []
 
+def get_listings_metadata():
+    try:
+        listings_meta = requests.get(upstream.get_listing_meta())
+        print(listings_meta.text)
+        return listings_meta.json()
+    except requests.ConnectionError as e:
+        return []
+
+def get_products_metadata():
+    try:
+        products_meta = requests.get(upstream.get_product_meta())
+        print(products_meta.text)
+        return products_meta.json()
+    except requests.ConnectionError as e:
+        return []
+
 @app.route('/healthz')
 def healthz():
     return 'OK'
@@ -35,7 +51,7 @@ def index():
     listings = get_listings()
     is_connect = os.environ.get('IS_CONNECT', 0)
     return render_template('index.html', prod_list=products, listings_list=listings,
-            is_connect=is_connect)
+            products_meta=get_products_metadata(), listings_meta=get_listings_metadata(), is_connect=is_connect)
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=8080)
--- upstream.py
+++ upstream.py
@@ -6,3 +6,10 @@ def get_listing_addr():
 
 def get_product_addr():
     return "{0}/product".format(os.getenv('PRODUCT_URI', 'http://localhost:10001'))
+
+def get_listing_meta():
+    return "{0}/metadata".format(os.getenv('LISTING_URI','http://localhost:10002'))
+
+
+def get_product_meta():
+    return "{0}/product/metadata".format(os.getenv('PRODUCT_URI', 'http://localhost:10001'))
